678439bc28eeaff6164852cdbcb57d3182b178b5
djyamunaq <dy.xcvi@gmail.com>
2024-02-28 13:20:32 +0100

    Update: install.sh

--- install.sh
+++ install.sh
@@ -30,8 +30,21 @@ sudo ./sgetpipe/temp pip install pandas >> /dev/null 2>&1
 echo -n "> Updating project modules "
 sudo ./sgetpipe/temp git submodule update --init --recursive >> /dev/null 2>&1
 
+# Install parallel-fastq-dump
+echo -n "> Decompressing parallel-fastq-dump "
+sudo ./sgetpipe/temp tar -xf ./sgetpipe/parallel-fastq-dump-0.6.7.tar.gz >> /dev/null 2>&1
+cd ./sgetpipe/parallel-fastq-dump-0.6.7
+echo -n "> Installing parallel-fastq-dump "
+sudo ../temp pip install -e . >> /dev/null 2>&1
+cd ../..
+
+# Install sratoolkit
+echo -n "> Decompressing and installing sra-toolkit "
+sudo ./sgetpipe/temp tar -xf sratoolkit.3.0.10-ubuntu64.tar.gz >> /dev/null 2>&1
+export PATH=./sgetpipe/sratoolkit.3.0.10-ubuntu64/bin/:${PATH}
+
 # pip install sgetpipe package
-echo "> Installing sgetpipe\n\n"
+echo "> Installing sgetpipe"
 if pip install -e .; then
     GREEN='\033[0;32m'
     echo -e "${GREEN}> Successfully installed package sgetpipe"
